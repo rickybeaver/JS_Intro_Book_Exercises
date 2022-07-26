a7ea8b2cbc9c8880e0af9284bfb2d3a7279046b9
Ricky Beaver <rickybeaver3@gmail.com>
2022-07-26 16:08:48 -0400

    Edit answers

--- 01_The_Basics/04.js
+++ 01_The_Basics/04.js
@@ -2,3 +2,4 @@
 // console.log('5' + 10);
 
 // The 5 is a string becaue of the quotations around it. Therefore, we are not adding the number 5 to the number 10. We are using the + operator between a string with the value of '5' and the number 10. Because a string is involved, all elements are turned to strings and concatenated.
+
